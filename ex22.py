ad60f9e3a86811d90f46a8d5c722171f1037a557
gulci <sebastian.gulczynski.poznan@gmail.com>
2015-10-06 12:09:32 +0200

    2015-10-06 time for chapter 31

---
+++ ex22.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+wiki = 2
+mela = 1
+
+if wiki > mela:
+	print "Wiki is older than Mela"
+elif wiki < mela:
+	print "Mela is older than Wiki"
+else:
+	print "Wiki and Mela are the same age"
+
+# może być kilka bloków elif, py wykonuje tylko pierwszy prawdziwy
